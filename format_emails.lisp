9b17c2e899085a90289f004245ab7b38c7df950b
Aptenodyte <graywater.stables@zoho.com>
2013-11-20 23:56:08 -0500

    Trying to make a program that will complete my emails on a pattern

---
+++ format_emails.lisp
@@ -0,0 +1,24 @@
+;; a simple script that reads an email prefix and prints a complete email
+
+#|
+; (defmacro my-for (start end)
+;   (when 
+|#
+
+(defparameter *my-data* (list "foo"))
+
+;(with-open-file (my-stream "input-list" :direction :input)
+;  (print my-stream))
+
+(defun email-expander (domain prefix &key target-stream)
+  "reformats an email to contain a given domain"
+  (princ (concatenate 'string prefix domain) target-stream)
+  (terpri target-stream))
+
+(defun mass-email-expander (domain prefix-list &key output-stream)
+  "format a bunch of emails to the same domain"
+  (with-open-file (my-stream input-file :direction :output)
+    (loop for i
+       from 0
+       do (email-expander domain (nth i prefix-list) :target-stream my-stream)
+       until (= i (length prefix-list)))))
